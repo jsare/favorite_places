82dc80f1a6f1e1d29afb45987d154dfe49aa5b68
Sergey Klimanoff <iamjsare@gmail.com>
2023-05-05 14:51:00 -0600

    Added getting the User's current location

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -60,10 +61,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               ),
             ),
             const SizedBox(height: 16),
-            ImageInput(onPickImage: (image) {
-              _selectedImage = image;
-            }),
-            const SizedBox(height: 16),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 8),
+            const LocationInput(),
+            const SizedBox(height: 8),
             ElevatedButton.icon(
               onPressed: _savePlace,
               icon: const Icon(Icons.add_location_alt_outlined),
