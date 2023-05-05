e787aad52a03a1532b055cba48341455123f422c
Sergey Klimanoff <iamjsare@gmail.com>
2023-05-05 11:46:17 -0600

    Added Places with Provider & Displaying Places

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:favorite_places/app.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 void main() {
-  runApp(const FavoritePlacesApp());
+  runApp(
+    const ProviderScope(child: FavoritePlacesApp()),
+  );
 }
